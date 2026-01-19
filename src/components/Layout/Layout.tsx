a3764297998683fb68dfec0d7ab4433e318cf45b
Marilo0 <marialenabarbopoulou@gmail.com>
2026-01-19 20:09:08 +0200

    chore: rename, deletes etc

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,22 +1,3 @@
-// import Header from "./Header.tsx"
-// import Footer from "./Footer.tsx"
-// import {Outlet} from "react-router";
-//
-// const Layout = () => {
-//     return (
-//         <>
-//             <Header/>
-//             {/*εβγαλα το px-24..προσθεσα το πτ-16*/}
-//             <main className="container pt-16 mx-auto min-h-[98vh]">
-//                 <Outlet/>
-//             </main>
-//             <Footer/>
-//         </>
-//     )
-// }
-//
-// export default Layout;
-
 import { Outlet } from "react-router";
 import Header from "./Header";
 import Footer from "./Footer";
