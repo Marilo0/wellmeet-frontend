87db781a75f535535f7687e067140650045d90e0
Marilo0 <marialenabarbopoulou@gmail.com>
2026-01-19 20:24:30 +0200

    fix(api): prevent sending false boolean filters to backend

--- src/services/api.activities.ts
+++ src/services/api.activities.ts
@@ -24,14 +24,29 @@ export async function  getActivities(
         pageNumber : pageNumber.toString(),
         pageSize : pageSize.toString(),
     })
+
     // Add filter parameters only if they exist
     if (filters) {
-        Object.entries(filters).forEach(([key, value]) => {
-            if (value !== undefined && value !== "") {
-                params.append(key, String(value));
-            }
-        });
+        if (filters) {
+            (Object.keys(filters) as (keyof ActivityFilters)[]).forEach((key) => {
+                const value = filters[key];
+
+                if (value === undefined || value === null) return;
+
+                if (typeof value === "boolean") {
+                    if (value) params.append(key, "true");
+                    return;
+                }
+
+                //  TS knows it's string
+                const v = value.trim();
+                if (v !== "") params.append(key, v);
+            });
+        }
+
+
     }
+
     const res = await fetch(`${API_URL}/activities?${params.toString()}`, {
         // method: 'GET',
         // headers: {"Content-Type": "application/json"},
